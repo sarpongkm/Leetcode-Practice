76c5b86b9f10c976fd55304097a15b926ba92f64
Kwabena Mensah Sarpong <sarpongkwabenamensah@gmail.com>
2025-08-30 23:25:03 -0400

    Add files via upload

---
+++ 0020_valid_parentheses/valid_parentheses_while.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def isValid(self, s):
+        while "()" in s or "[]" in s or "{}" in s:
+            s = s.replace("()", "")
+            s = s.replace("[]", "")
+            s = s.replace("{}", "")
+        return s == ""
